fe731d26354ea616594b48bfdf5e7427f433b162
gooooloo <qidu.lin@gmail.com>
2012-06-27 22:25:42 +0800

    first version

--- src/com/qidu/lin/time/accumulater/TADataCenter.java
+++ src/com/qidu/lin/time/accumulater/TADataCenter.java
@@ -5,22 +5,27 @@
 
 package com.qidu.lin.time.accumulater;
 
+import java.util.Calendar;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
 public class TADataCenter
 {
 	private static final String TAG = "abc";
-	private static final String TAG2 = "def";
+	private static final String TAG_IS_TIME_COMSUMING_FLAG = "com.qidu.lin.timeAccumulate.TAG_IS_TIME_COMSUMING_FLAG";
+	private static final String TAG_TIME_ACCUMULATE = "com.qidu.lin.timeAccumulate.TAG_TIME_ACCUMULATE";
+	private static final String TAG_LAST_TOGGLE_TIME = "com.qidu.lin.timeAccumulate.TAG_LAST_TOGGLE_TIME";
 
-	public static boolean getOnFlag(Context context)
+	static boolean getOnFlag(Context context)
 	{
-		return TADataCenter.getSP(context).getBoolean(TAG2, false);
+
+		return TADataCenter.getSP(context).getBoolean(TAG_IS_TIME_COMSUMING_FLAG, false);
 	}
 
-	public static void setOnFlag(Context context, boolean on)
+	private static void setOnFlag(Context context, boolean on)
 	{
-		TADataCenter.getSP(context).edit().putBoolean(TAG2, on).commit();
+		TADataCenter.getSP(context).edit().putBoolean(TAG_IS_TIME_COMSUMING_FLAG, on).commit();
 	}
 
 	private static SharedPreferences getSP(Context context)
@@ -31,7 +36,44 @@ public class TADataCenter
 
 	public static void Toggle(Context context)
 	{
-		setOnFlag(context, !getOnFlag(context));
+		boolean beforeIsOn = getOnFlag(context);
+
+		Calendar x = Calendar.getInstance();
+		long y = x.getTimeInMillis();
+
+		if (beforeIsOn)
+		{
+			long yy = getLastTime(context);
+			long yyy = y - yy;
+			setAccumulate(context, yyy + getAccumulate(context));
+		}
+		setLastTime(context, y);
+		setOnFlag(context, !beforeIsOn);
+
+	}
+
+	public static long getAccumulate(Context context)
+	{
+		// TODO Auto-generated method stub
+
+		long x =  getSP(context).getLong(TAG_TIME_ACCUMULATE, 0);
+		return x;
+	}
+
+	private static void setAccumulate(Context context, long yyy)
+	{
+		// TODO Auto-generated method stub
+		getSP(context).edit().putLong(TAG_TIME_ACCUMULATE, yyy).commit();
+	}
+
+	private static long getLastTime(Context context)
+	{
+		return getSP(context).getLong(TAG_LAST_TOGGLE_TIME, Calendar.getInstance().getTimeInMillis());
+	}
+
+	private static void setLastTime(Context context, long y)
+	{
+		getSP(context).edit().putLong(TAG_LAST_TOGGLE_TIME, y).commit();
 	}
 
 }
--- src/com/qidu/lin/time/accumulater/TAMain.java
+++ src/com/qidu/lin/time/accumulater/TAMain.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TAMain extends Activity
 {
@@ -43,8 +44,28 @@ public class TAMain extends Activity
 	{
 
 		Button x = (Button) this.findViewById(R.id.btn);
-		x.setText(TADataCenter.getOnFlag(this) ? "set On" : "set Off");
+		x.setText(TADataCenter.getOnFlag(this) ? "set Off" : "set on");
+		this.updateText();
 
 	}
 
+	private void updateText()
+	{
+		TextView tv = (TextView) this.findViewById(R.id.tv);
+
+		if (TADataCenter.getOnFlag(this))
+		{
+			tv.setText("¼ÆÊ±ÖÐ");
+		}
+		else
+		{
+			long x = TADataCenter.getAccumulate(this);
+			long sec = x / 1000;
+			long min = sec / 60;
+			sec = sec % 60;
+			long hour = min / 60;
+			min = min % 60;
+			tv.setText("accumulate : " + hour + " hours " + min + " minutes " + sec + " seconds.");
+		}
+	}
 }
