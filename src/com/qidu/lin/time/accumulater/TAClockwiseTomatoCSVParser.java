12ff5bc78bb9f170ab69f63069ae00b689d9a95b
gooooloo <qidu.lin@gmail.com>
2013-07-06 11:45:04 +0800

    show tomato correctly

--- src/com/qidu/lin/time/accumulater/TAClockwiseTomatoCSVParser.java
+++ src/com/qidu/lin/time/accumulater/TAClockwiseTomatoCSVParser.java
@@ -10,11 +10,11 @@ public class TAClockwiseTomatoCSVParser
 	public static List<TATomato> parse(Uri uri)
 	{
 		ArrayList<TATomato> list = new ArrayList<TATomato>();
-		
+
 		TACSVParser csvParser = new TACSVParser(uri);
 
 		long lineCnt = 0;
-		for (String [] y : csvParser.getMyEntries())
+		for (String[] y : csvParser.getMyEntries())
 		{
 			lineCnt++;
 			if (lineCnt == 1)
@@ -22,11 +22,18 @@ public class TAClockwiseTomatoCSVParser
 				// pass for header line.
 				continue;
 			}
-			
-			TATomato tomato = new TATomato(y[5], y[6]);
+
+			TATomato tomato = new TATomato(getStartOrEndTimeMs(y[5]), getStartOrEndTimeMs(y[6]));
 			list.add(tomato);
 		}
-		
+
 		return list;
+	}
+
+	private static long getStartOrEndTimeMs(String timeStr)
+	{
+		// assumption : the time in clockwise tomato are in "Seconds" rather
+		// than in "MillionSeconds".
+		return Long.valueOf(timeStr.trim()) * 1000;
 	}
 }
--- src/com/qidu/lin/time/accumulater/TATomato.java
+++ src/com/qidu/lin/time/accumulater/TATomato.java
@@ -1,31 +1,34 @@
 package com.qidu.lin.time.accumulater;
 
+import java.util.Calendar;
+
 public class TATomato
 {
-	public final int duration;
-	public final long start;
-	public final long end;
-	public final int minutes;
-	public final int seconds;
+	private final int durationMs;
+	public final long startMs;
+	public final long endMs;
 
-	public TATomato(String start, String end)
+	public TATomato(long startMs, long endMs)
 	{
-		this.start = Long.valueOf(start.trim());
-		this.end = Long.valueOf(end.trim());
+		this.startMs = startMs;
+		this.endMs = endMs;
 
-		duration = (int) (this.end - this.start);
-		seconds = duration % 60;
-		minutes = duration / 60;
+		durationMs = (int) (this.endMs - this.startMs);
 	}
 
 	public String getDurationString()
 	{
+		int durationSec = durationMs / 1000;
+		int seconds = durationSec % 60;
+		int minutes = durationSec / 60;
 		return "" + minutes + ":" + seconds;
 	}
-	
+
 	public String getStartTimeString()
 	{
-		return ""+start;
+		Calendar x = Calendar.getInstance();
+		x.setTimeInMillis(startMs);
+		return x.getTime().toLocaleString();
 	}
 
 }
