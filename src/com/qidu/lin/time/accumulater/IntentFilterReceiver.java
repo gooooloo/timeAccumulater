92f7b08ebf38291da4f48e03c3ac202fd4e78520
gooooloo <qidu.lin@gmail.com>
2013-07-08 09:17:41 +0800

    filter UI

--- src/com/qidu/lin/time/accumulater/IntentFilterReceiver.java
+++ src/com/qidu/lin/time/accumulater/IntentFilterReceiver.java
@@ -139,9 +139,10 @@ public class IntentFilterReceiver extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_intent_filter_receiver, menu);
 		return true;
 	}
+	
+	
 
 }
