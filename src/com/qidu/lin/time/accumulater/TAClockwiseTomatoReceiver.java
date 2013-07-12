5cbf8e06a52658bf50802ce7a30bbead205ab131
gooooloo <qidu.lin@gmail.com>
2013-07-12 08:26:57 +0800

    refactor

--- src/com/qidu/lin/time/accumulater/TAClockwiseTomatoReceiver.java
+++ src/com/qidu/lin/time/accumulater/TAClockwiseTomatoReceiver.java
@@ -38,18 +38,72 @@ import android.widget.Toast;
 
 public class TAClockwiseTomatoReceiver extends Activity
 {
-	protected static final int selectdode = 0;
-	private static final String DURATION = "DURATION";
-	private static final String TOMATO_ID = "TOMATO_ID";
-
 	private class FilterRules
 	{
 		public boolean unaccumalatedOnly = false;
 		public boolean within2DaysOnly = false;
 	}
 
+	private static final String DURATION = "DURATION";
+	private static final int selectdode = 0;
+
+	private static final String TOMATO_ID = "TOMATO_ID";
+
 	FilterRules filterRules = new FilterRules();
 	List<TATomato> tomatoListReverse = null;
+
+	private boolean filterOff(TATomato tomato, FilterRules filterRules)
+	{
+		if (filterRules.unaccumalatedOnly)
+		{
+			if (TATomatoPersistence.getProjectName(this, tomato.getId()) != null)
+			{
+				return true;
+			}
+		}
+
+		if (filterRules.within2DaysOnly)
+		{
+			long msUntilNow = System.currentTimeMillis() - tomato.startMs;
+			int msPerDay = 1000 * 60 * 60 * 24;
+			if (msUntilNow > msPerDay * 2)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private void filterTomatoListByUISettings(List<TATomato> tomatoListReverse2, FilterRules filterRules)
+	{
+		for (int i = tomatoListReverse2.size() - 1; i >= 0; i--)
+		{
+			if (filterOff(tomatoListReverse2.get(i), filterRules))
+			{
+				tomatoListReverse2.remove(i);
+			}
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (requestCode == TAMain.selectdode && resultCode == Activity.RESULT_OK)
+		{
+			int projectId = data.getIntExtra(TASelect.ID, 0);
+			int duration = data.getIntExtra(DURATION, 0);
+			long tomatoId = data.getLongExtra(TOMATO_ID, 0);
+			String projectName = TADataCenter.ProjectCenter.getProjectNameById(this, projectId);
+			TADataCenter.addPastTimeToAccumulate(this, projectName, duration);
+			TATomatoPersistence.save(this, tomatoId, projectName);
+
+			Toast.makeText(this, "time accumulated!", Toast.LENGTH_SHORT).show();
+
+			updateUI();
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -59,6 +113,70 @@ public class TAClockwiseTomatoReceiver extends Activity
 
 		tomatoListReverse = parseTomatoListReverse();
 		updateUI();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_intent_filter_receiver, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+		case R.id.menu_unaccumulatedOnly:
+			filterRules.unaccumalatedOnly = true;
+			updateUI();
+			break;
+		case R.id.menu_within2daysonly:
+			filterRules.within2DaysOnly = true;
+			updateUI();
+			break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	protected void onTomatoClicked(TATomato y)
+	{
+		Intent intent = new Intent(this, TASelect.class);
+		intent.putExtra(DURATION, y.getDurationMs());
+		intent.putExtra(TOMATO_ID, y.getId());
+		startActivityForResult(intent, selectdode);
+	}
+
+	private List<TATomato> parseTomatoListReverse()
+	{
+		Intent intent = getIntent();
+		assert (intent.getType().equalsIgnoreCase("text/plain"));
+		Uri stream = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+
+		List<TATomato> tomatoListReverse = null;
+		try
+		{
+			tomatoListReverse = TAClockwiseTomatoCSVParser.parseInReverse(stream);
+		}
+		catch (IOException e)
+		{
+			String content = intent.getStringExtra(Intent.EXTRA_TEXT);
+			String regularExpression = "((\\d+):(\\d+)\\.\\d+)";
+			Matcher matcher = Pattern.compile(regularExpression).matcher(content);
+			if (matcher.find())
+			{
+				MatchResult results = matcher.toMatchResult();
+				String min = results.group(2);
+				String sec = results.group(3);
+				int durationMs = (Integer.valueOf(min) * 60 + Integer.valueOf(sec)) * 1000;
+				long curMs = System.currentTimeMillis();
+				tomatoListReverse = new ArrayList<TATomato>();
+				tomatoListReverse.add(new TATomato(curMs - durationMs, curMs));
+			}
+		}
+
+		return tomatoListReverse;
 	}
 
 	private void updateUI()
@@ -97,124 +215,6 @@ public class TAClockwiseTomatoReceiver extends Activity
 			});
 			index--;
 		}
-	}
-
-	private List<TATomato> parseTomatoListReverse()
-	{
-		Intent intent = getIntent();
-		assert (intent.getType().equalsIgnoreCase("text/plain"));
-		Uri stream = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-
-		List<TATomato> tomatoListReverse = null;
-		try
-		{
-			tomatoListReverse = TAClockwiseTomatoCSVParser.parseInReverse(stream);
-		}
-		catch (IOException e)
-		{
-			String content = intent.getStringExtra(Intent.EXTRA_TEXT);
-			String regularExpression = "((\\d+):(\\d+)\\.\\d+)";
-			Matcher matcher = Pattern.compile(regularExpression).matcher(content);
-			if (matcher.find())
-			{
-				MatchResult results = matcher.toMatchResult();
-				String min = results.group(2);
-				String sec = results.group(3);
-				int s = Integer.valueOf(min) * 60 + Integer.valueOf(sec);
-				s *= 1000;
-				long curMs = System.currentTimeMillis();
-				tomatoListReverse = new ArrayList<TATomato>();
-				tomatoListReverse.add(new TATomato(curMs - s, curMs));
-			}
-		}
-
-		return tomatoListReverse;
-	}
-
-	private void filterTomatoListByUISettings(List<TATomato> tomatoListReverse2, FilterRules filterRules)
-	{
-		for (int i = tomatoListReverse2.size() - 1; i >= 0; i--)
-		{
-			if (filterOff(tomatoListReverse2.get(i), filterRules))
-			{
-				tomatoListReverse2.remove(i);
-			}
-		}
-	}
-
-	private boolean filterOff(TATomato tomato, FilterRules filterRules)
-	{
-		if (filterRules.unaccumalatedOnly)
-		{
-			if (TATomatoPersistence.getProjectName(this, tomato.getId()) != null)
-			{
-				return true;
-			}
-		}
-
-		if (filterRules.within2DaysOnly)
-		{
-			long msUntilNow = System.currentTimeMillis() - tomato.startMs;
-			int msPerDay = 1000 * 60 * 60 * 24;
-			if (msUntilNow > msPerDay * 2)
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	protected void onTomatoClicked(TATomato y)
-	{
-		Intent intent = new Intent(this, TASelect.class);
-		intent.putExtra(DURATION, y.getDurationMs());
-		intent.putExtra(TOMATO_ID, y.getId());
-		startActivityForResult(intent, selectdode);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		if (requestCode == TAMain.selectdode && resultCode == Activity.RESULT_OK)
-		{
-			int projectId = data.getIntExtra(TASelect.ID, 0);
-			int duration = data.getIntExtra(DURATION, 0);
-			long tomatoId = data.getLongExtra(TOMATO_ID, 0);
-			String projectName = TADataCenter.ProjectCenter.getProjectNameById(this, projectId);
-			TADataCenter.addPastTimeToAccumulate(this, projectName, duration);
-			TATomatoPersistence.save(this, tomatoId, projectName);
-
-			Toast.makeText(this, "time accumulated!", Toast.LENGTH_SHORT).show();
-
-			updateUI();
-		}
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		getMenuInflater().inflate(R.menu.activity_intent_filter_receiver, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-		case R.id.menu_unaccumulatedOnly:
-			filterRules.unaccumalatedOnly = true;
-			updateUI();
-			break;
-		case R.id.menu_within2daysonly:
-			filterRules.within2DaysOnly = true;
-			updateUI();
-			break;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 
 }
